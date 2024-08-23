5ef253605fa96726b5d0f338adac17bade89d559
meywy <meywyy@gmail.com>
2024-08-23 15:46:54 +0200

    Initial commit

---
+++ src/main/java/dev/meywy/meyAPI/MeyAPI.java
@@ -0,0 +1,17 @@
+package dev.meywy.meyAPI;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MeyAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
