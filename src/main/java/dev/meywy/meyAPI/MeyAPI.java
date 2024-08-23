586e956d2f049e45407b53cc8f86cb167b051305
meywy <meywyy@gmail.com>
2024-08-23 15:57:41 +0200

    update

--- src/main/java/dev/meywy/meyAPI/MeyAPI.java
+++ src/main/java/dev/meywy/meyAPI/MeyAPI.java
@@ -4,5 +4,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MeyAPI extends JavaPlugin {
 
+    @Override
+    public void onEnable() {
+
+    }
+
     public static String getStatus() { return "It's working finally"; }
 }
